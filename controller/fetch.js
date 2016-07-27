7849113c788b1477b3735365a63917305727e644
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-27 22:36:27 +0100

    fixed validation

--- controller/fetch.js
+++ controller/fetch.js
@@ -19,7 +19,7 @@ var fetch = function onFetch(req, res, next) {
 	default:
 		model = Models.ProductModel;
 		query = { type: req.params.model };
-		validation = process.env.APPLICATION_PRODUCTS.split(',').indexOf(req.params.model) === -1;
+		validation = process.env.APPLICATION_PRODUCTS.split(',').indexOf(req.params.model) >= 0;
 		pagination.sort = (req.query.sort) ? req.query.sort.replace(/,/g, ' ') : 'user';
 		if (!validation) return res.status(200).json({ items: [], found: {} });
 		break;
